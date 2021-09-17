b7198b0572cd13aa1914bec31936945e2869ef1f
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-09-16 22:39:30 -0700

    Bump template

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-copier gh:blakeNaccarato/copier-python . -r cffe5a5
+copier gh:blakeNaccarato/copier-python . -r 665ef69
 py -3.9 -m venv .venv
 .venv/Scripts/activate
 pip install -U pip  # throws [WinError 5], but still works on its own
