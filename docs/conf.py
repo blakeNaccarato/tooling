bb8a68ba950fb3b56edce2d907d6fa70a199f87a
blakeNaccarato <blake.naccarato@gmail.com>
2022-09-10 18:01:05 -0700

    Reformat conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,13 +3,10 @@ from datetime import date
 from myst_parser import __version__
 
 project = "Project"
-copyright = f"{date.today().year}, Blake Naccarato"
+copyright = f"{date.today().year}, Blake Naccarato"  # noqa: A001
 version = __version__
 master_doc = "index"
 language = "en"
-extensions = [
-    "myst_parser",
-    "sphinx_design"
-]
+extensions = ["myst_parser", "sphinx_design"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_book_theme"
